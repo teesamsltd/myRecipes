4b31192ab452bcde7f0ad603b4d8f813365495dc
Samuel Omopariola <admin@teesams.com>
2015-06-24 05:48:58 +0000

    create chef and recipe association and test

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,3 +1,10 @@
 class Chef < ActiveRecord::Base
+  has_many :recipes
+  before_save { self.email = email.downcase }
+  validates :chefname, presence: true, length: { minimum: 3, maximum: 40 }
+  VALID_EMAIL_REGEX = /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ # /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ #/\A[\w+\-.]+@[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 105 },
+                                    uniqueness: { case_sensitive: false },
+                                    format: { with: VALID_EMAIL_REGEX }
   
 end
